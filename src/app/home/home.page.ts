4b08f844bbfdf292f39c47c5d032d88bf7bb24ad
Belon112 <mnabil2020@gmail.com>
2021-08-12 16:03:21 +0800

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  slider: any;
+  slideOptions = {
+  initialSlide: 0,
+  slidesPerView: 1,
+  autoplay: true
+};
 
-  constructor() {}
-
+constructor() {}
+slideChanged()
+{
+   this.slider.stopAutoplay(); //this code for slide after page change
+   }
 }
+
+
